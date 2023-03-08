dfc38398b55017a3d7fd62127d5ab7027186a3c8
Vidhya1810 <you@example.com>
2023-03-08 21:36:58 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here
+def  Vidhya(request):
+    return HttpResponse('<h1>Vidhya is a cute girl<h1>')
+
